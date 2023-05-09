1c35df0bd260f71957a3c82d806a08eb8e39b050
A.Safwat <64287231+19B2@users.noreply.github.com>
2023-05-09 19:32:35 +0200

    delete button fixed

--- inc/delete-post.php
+++ inc/delete-post.php
@@ -1,27 +1,12 @@
 <?php
 session_start();
 
-if (isset($_GET['topic']) && isset($_GET['post']) && isset($_GET['by']) && isset($_SESSION['id']) && $_GET['by'] == $_SESSION['id'] && isset($_GET['topic'])) {
+if (isset($_GET['a_id'])) {
     require 'dbh.inc.php';
-
-    $post = $_GET['post'];
-    $topic = $_GET['topic'];
-
-    $sql = "DELETE FROM answers WHERE id = ?";
-    $stmt = mysqli_stmt_init($db);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("Location: ../posts.php?topic=".$topic."&error=sqlerror");
-        exit();
-    } else {
-        mysqli_stmt_bind_param($stmt, "i", $post);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-        mysqli_close($db);
-        header("Location: ../posts.php?topic=".$topic);
-        exit();
-    }
-
-} else {
-    header("Location: ../posts.php?topic=".$topic);
-    exit();
+    $a_id = $_GET['a_id'];
+    $q_id = $_GET['q_id'];
+    $sql = "DELETE FROM answers WHERE a_id = $a_id";
+    mysqli_query($db, $sql);
+    header("Location: ../posts.php?topic=$q_id");
 }
+?>
